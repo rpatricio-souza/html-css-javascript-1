ccb18c86cdfa54a23bf3eac38145150fb7d75f21
rpatricio-souza <50207058+rpatricio-souza@users.noreply.github.com>
2020-05-08 10:13:03 -0300

    Aula 36

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -5,3 +5,9 @@ function mudaFoto(foto){
 function mudaIframe(iframe){
     document.getElementById("frame-spec").src = iframe;
 }
+
+function calcTotal(){
+    var qtd=parseInt(document.getElementById('campo-qtd').value);
+    total=qtd*1500.00;
+    document.getElementById('campo-total').value=total;
+}
